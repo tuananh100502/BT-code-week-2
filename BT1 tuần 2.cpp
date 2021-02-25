7557c0236941ca7c321821844a7ee4aef051c539
tuananh100502 <78184363+tuananh100502@users.noreply.github.com>
2021-02-25 22:02:27 +0700

    Update BT1 tuần 2.cpp

--- BT1 tuần 2.cpp
+++ BT1 tuần 2.cpp
@@ -1,13 +1,13 @@
-1.	while (n<20); // dau ";" dang sau while nen chuong trinh loi
+1.	while (n<20); 
    cout << n++ <<endl;
 2.	for (int i = 1, i <= 8, ++i)
-   cout << 1.0/i << endl; // khai bao dinh dang i la int nen phep chia khong ep kieu se khong co ket qua chinh xac
+   cout << 1.0/i << endl; 
 3.	int n = 10;
    do
- 	      cout << 1.0/n ; // thieu 2 dau ngoac nne lenh do sai
+ 	      cout << 1.0/n ; 
                   n++;
                while (n < 20);
-4.	for (int i=10; i<20; ) { // vong lap khong chay do tu dau i=10 da <20 
+4.	for (int i=10; i<20; ) { 
    cout << i*i <<endl;
    i--;
 }
